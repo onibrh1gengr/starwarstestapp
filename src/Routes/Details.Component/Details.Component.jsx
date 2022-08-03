f1fb4708c935de64ae68176d1fbcf84a675974d2
onibrh1gengr <onib@genesisengr.com>
2022-08-03 17:08:36 -0400

    did 80%

--- src/Routes/Details.Component/Details.Component.jsx
+++ src/Routes/Details.Component/Details.Component.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import {useLocation} from 'react-router-dom'
+import axios from 'axios'
 
-function DetailsComponent() {
+function DetailsComponent(props) {
+  const { state } = useLocation();
+  const { characters, title, director, opening_crawl, producer } = state.film;
+  const [ characterDetails, setCharacterDetails] = useState([]);
+
+
+  useEffect(()=>{
+    const fetchedObjArray = characters.map(async (value)=>{
+      let singleObj = {}
+      await fetch(value).then(res=>res.json()).then(res=>singleObj = res)
+      return singleObj
+      })
+      
+      Promise.all(fetchedObjArray).then((value)=>{
+      // this
+      setCharacterDetails(value);
+      }).catch(err=>console.log(err))
+  },[])
+
+
+
+
+
+
+if(characterDetails.length === 0){
   return (
-    <div>DetailsComponent</div>
+    <div>Loading...</div>
   )
+} else{
+  return(
+    <>
+    {console.log(characterDetails)}
+    </>
+  )
+}
 }
 
 export default DetailsComponent
--- src/Routes/Master.component/Master.component.jsx
+++ src/Routes/Master.component/Master.component.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import "./Master.component.styles.scss"
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import Star from "../../assets/starwars.webp"
 
 function MasterComponent() {
+  const [filmsList, setFilmsList] = useState([])
+  const [peopleList, setPeopleList] = useState([])
+  useEffect(()=> {
+    axios.get(`https://swapi.dev/api/films/`)
+    .then(res => {
+      const films = res.data;
+      const {results} = films;
+      setFilmsList(results);
+    })
+    axios.get(`https://swapi.dev/api/people/`)
+    .then(res => {
+      const films = res.data;
+      const {results} = films;
+      setPeopleList(results);
+    })
+  },[])
+
+  const navigate = useNavigate();
+
   return (
-    <div className='card-container '></div>
+    <div className='card-container'>
+      {filmsList.length > 0 && (
+      <div className='film-card-container'>
+        {filmsList.map( (film) => 
+        <div 
+        onClick={() => navigate('/details', { state: { film , people: peopleList} })} 
+        key={film.episode_id}
+        >
+          <div className='film-card'>
+            <img src={Star} width={100} height={100} alt={"starwars"} />
+            <p><strong>{film.title}</strong></p>
+            <small className='film-desc'>{film.opening_crawl}</small>
+          </div> 
+        </div>
+        )}
+      </div>
+      )}
+    </div>
   )
 }
 
